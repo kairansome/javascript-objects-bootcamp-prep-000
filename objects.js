e45a34c4485864f52c21f0fa481392ccfdfd2d4f
Kai Emerson Ransome <ransomekai@gmail.com>
2018-02-26 04:56:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new Object({artistName: 'songtitle'})
 
 function updatePlaylist (obj, artistName, songTitle) {
-   obj.artistName = songTitle 
+   obj[artistName] = songTitle 
   return obj
   }
 
